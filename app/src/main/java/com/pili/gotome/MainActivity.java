c8f3811085b0f58ff2412a90169744bcc7cf3c9f
yiyeshua <784148786@qq.com>
2017-07-25 21:11:39 +0800

    1

--- app/src/main/java/com/pili/gotome/MainActivity.java
+++ app/src/main/java/com/pili/gotome/MainActivity.java
@@ -1,13 +1,24 @@
 package com.pili.gotome;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.content)
+    FrameLayout content;
+    @BindView(R.id.rg)
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
