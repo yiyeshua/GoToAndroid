4e9aeba3173b02fb2bf61f4ffbc7abec3fab3e8a
yiyeshua <784148786@qq.com>
2017-07-26 22:21:01 +0800

    3

---
+++ app/src/main/java/com/pili/gotome/HomeFragment.java
@@ -0,0 +1,23 @@
+package com.pili.gotome;
+
+import com.pili.basemodel.base.BaseFragment;
+
+/**
+ * Created by lhw on 2017/7/25.
+ */
+public class HomeFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.frag_layout_home;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void lazyFetchData() {
+
+    }
+}
